21ac3942a6e6f838b89a9de6c35cf2f6205b1ed7
Alexandr <ekkib@mail.ru>
2025-02-24 14:58:39 +0300

    ...

--- src/main/java/web/model/CarsService.java
+++ src/main/java/web/model/CarsService.java
@@ -8,6 +8,11 @@ import java.util.List;
 
 
 public interface CarsService {
-    List<Cars> carsList = new ArrayList<>();
+      List<Cars> carsList = List.of(
+            new Cars(1000,"MB", 100),
+        new Cars(1001,"MB", 100),
+        new Cars(1002,"MB", 102),
+        new Cars(1003,"MB", 104),
+        new Cars(1004,"MB", 102));
    List<Cars> carsCount(Integer number);
 }
--- src/main/java/web/model/CarsServiceImpl.java
+++ src/main/java/web/model/CarsServiceImpl.java
@@ -10,13 +10,7 @@ public class CarsServiceImpl implements CarsService{
 
     @Override
     public  List<Cars> carsCount(Integer number) {
-        carsList.add(new Cars(1000,"MB", 100));
-        carsList.add(new Cars(1001,"MB", 100));
-        carsList.add(new Cars(1002,"MB", 102));
-        carsList.add(new Cars(1003,"MB", 104));
-        carsList.add(new Cars(1004,"MB", 102));
-
-    return (number == null || number <= 0 || number >= 5)
+        return (number == null || number <= 0 || number >= 5)
             ? carsList
             : carsList.stream().limit(number).collect(Collectors.toList());
     }
